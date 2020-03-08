e0db8ba98e169a573f35cd5a1564e03965906e33
tianqi77 <e0323090@u.nus.edu>
2020-03-09 03:51:13 +0800

    update message

---
+++ src/main/java/seedu/address/commons/petCore/Messages.java
@@ -0,0 +1,8 @@
+package seedu.address.commons.petCore;
+
+public class Messages {
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_PET_DISPLAYED_INDEX = "The pet index provided is invalid";
+    public static final String MESSAGE_PETS_LISTED_OVERVIEW = "%1$d pets listed!";
+}
