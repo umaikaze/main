fd0f94c12d9d3977552c67aa116ef959799db738
Chen Shenghao <chen.shenghao@u.nus.edu>
2020-03-12 19:16:34 +0800

    Add JsonAdaptedFood

---
+++ src/main/java/seedu/address/storage/JsonAdaptedFood.java
@@ -0,0 +1,59 @@
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.pet.Food;
+
+/**
+ * Jackson-friendly version of {@link Food}.
+ */
+class JsonAdaptedFood {
+
+    private final String foodName;
+    private final Integer foodAmount;
+
+    /**
+     * Constructs a {@code JsonAdaptedFood} with the given {@code foodName}.
+     */
+    @JsonCreator
+    public JsonAdaptedFood(String foodName, Integer foodAmount) {
+        this.foodName = foodName;
+        this.foodAmount = foodAmount;
+    }
+
+    /**
+     * Converts a given {@code Food} into this class for Jackson use.
+     */
+    public JsonAdaptedFood(Food source) {
+        foodName = source.foodName;
+        foodAmount = source.foodAmount;
+    }
+
+    @JsonValue
+    public String getFoodName() {
+        return foodName;
+    }
+
+    @JsonValue
+    public String getFoodAmount() {
+        return foodName;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted tag object into the model's {@code Food} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted tag.
+     */
+    public Food toModelType() throws IllegalValueException {
+        if (!Food.isValidFoodName(foodName)) {
+            throw new IllegalValueException(Food.MESSAGE_CONSTRAINTS);
+        }
+        if (!Food.isValidFoodAmount(foodAmount)) {
+            throw new IllegalValueException(Food.MESSAGE_AMOUNT_CONSTRAINTS);
+        }
+        return new Food(foodName, foodAmount);
+    }
+
+}
