eab7794793afd7906cf832022c8722bf30258652
Zhu-Ruicong <traviszhuruicong@gmail.com>
2020-03-14 16:09:55 +0800

    no message

--- src/main/java/seedu/address/petui/PetListPanel.java
+++ src/main/java/seedu/address/petui/PetListPanel.java
@@ -1,13 +1,10 @@
 package seedu.address.petui;
 
-import java.util.logging.Logger;
-
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.pet.Pet;
 
 /**
@@ -15,7 +12,6 @@ import seedu.address.model.pet.Pet;
  */
 public class PetListPanel extends UiPart<Region> {
     private static final String FXML = "PetListPanel.fxml";
-    private final Logger logger = LogsCenter.getLogger(PetListPanel.class);
 
     @FXML
     private ListView<Pet> petListView;
