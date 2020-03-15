f8b6abff96aad4112419262659575302d6e3822b
Chen Shenghao <chen.shenghao@u.nus.edu>
2020-03-15 20:00:31 +0800

    Refactor TypicalIndexes for PSH

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -9,8 +9,4 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_PERSON = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_PERSON = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_PERSON = Index.fromOneBased(3);
-
-    public static final Index INDEX_FIRST_PET = Index.fromOneBased(1);
-    public static final Index INDEX_SECOND_PET = Index.fromOneBased(2);
-    public static final Index INDEX_THIRD_PET = Index.fromOneBased(3);
 }
