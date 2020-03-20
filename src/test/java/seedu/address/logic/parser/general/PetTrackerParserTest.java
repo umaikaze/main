aeb89be81ea7c929b3ed3c82fe386ef4b19b7a8a
Dylan Lim <60003775+limdylan@users.noreply.github.com>
2020-03-21 06:03:24 +0800

    Add definition for new methods in test Model stubs

--- src/test/java/seedu/address/logic/parser/general/PetTrackerParserTest.java
+++ src/test/java/seedu/address/logic/parser/general/PetTrackerParserTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.parser.pet;
+package seedu.address.logic.parser.general;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
