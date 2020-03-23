c61c62553c27fad98adab6359c6deed9ee11222e
Zhu-Ruicong <traviszhuruicong@gmail.com>
2020-03-24 02:09:17 +0800

    fix bug in test

--- src/test/java/seedu/address/logic/commands/pet/AddPetCommandTest.java
+++ src/test/java/seedu/address/logic/commands/pet/AddPetCommandTest.java
@@ -21,7 +21,6 @@ import seedu.address.model.Model;
 import seedu.address.model.PetTracker;
 import seedu.address.model.ReadOnlyPetTracker;
 import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.pet.FoodCollection;
 import seedu.address.model.pet.Name;
 import seedu.address.model.pet.Pet;
 import seedu.address.model.slot.Slot;
@@ -183,16 +182,6 @@ public class AddPetCommandTest {
         @Override
         public void updateFilteredSlotList(Predicate<Slot> predicate) {
             throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ObservableList<FoodCollection> getFilteredFoodCollectionList() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void updateFilteredFoodCollectionList(Predicate<FoodCollection> predicate) {
-
         }
 
         @Override
