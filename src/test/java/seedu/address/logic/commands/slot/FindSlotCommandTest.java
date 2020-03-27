1690256471cacf675e8a04202ec95c1463d589d3
tianqi77 <e0323090@u.nus.edu>
2020-03-27 14:25:02 +0800

    no message

--- src/test/java/seedu/address/logic/commands/slot/FindSlotCommandTest.java
+++ src/test/java/seedu/address/logic/commands/slot/FindSlotCommandTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_SLOTS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_COCO;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_GARFIELD;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.assertFindCommandSuccess;
 import static seedu.address.logic.parser.general.CliSyntax.PREFIX_NAME;
 import static seedu.address.testutil.pet.TypicalPets.getTypicalModelManager;
 
