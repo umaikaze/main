ab7fd880bad6bb1b684b41ef041364fc4c1cce2a
tianqi77 <e0323090@u.nus.edu>
2020-03-29 02:42:46 +0800

    minor change

--- src/main/java/seedu/address/logic/commands/slot/FindSlotCommand.java
+++ src/main/java/seedu/address/logic/commands/slot/FindSlotCommand.java
@@ -21,8 +21,8 @@ public class FindSlotCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all slots where pet name matches exactly "
             + "(case-insensitive) or slots that is within the date specified (ignoring timing) and displays them as a "
             + "list with index numbers.\n"
-            + "Parameters: [n/PETNAME] [t/DATE]...\n"
-            + "Example: " + COMMAND_WORD + " n/garfield t/10/11/2020";
+            + "Parameters: [n/PET NAME] [t/DATE [MORE DATES]...]\n"
+            + "Example: " + COMMAND_WORD + " n/garfield t/10/11/2020 5/12/2020";
 
     public static final String MESSAGE_EMPTY_NAME_FIELD = "n/ should not be followed by an empty entry!";
 
