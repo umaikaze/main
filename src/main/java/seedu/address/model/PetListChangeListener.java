a8c5d0d5575bbacad32206c5a8c2f92907342962
umaikaze <zmj10086@gmail.com>
2020-03-28 21:29:47 +0800

    Skeleton code for updating slots upon petList edit

---
+++ src/main/java/seedu/address/model/PetListChangeListener.java
@@ -0,0 +1,31 @@
+package seedu.address.model;
+
+import java.util.List;
+
+import javafx.collections.ListChangeListener;
+import seedu.address.model.pet.Pet;
+
+/**
+ * Listener class to look out for changes in the pet list and use to modify schedule correspondingly
+ */
+public class PetListChangeListener implements ListChangeListener<Pet> {
+
+    @Override
+    public void onChanged (Change<? extends Pet> petChange) {
+        List<? extends Pet> removed = petChange.getRemoved();
+        List<? extends Pet> added = petChange.getAddedSubList();
+
+        assert (removed.size() < 2 && added.size() < 2);
+
+        updateSlotsDueToPetEdit(removed.get(0), added.get(0));
+        removeExcessSlot(removed.get(0));
+    }
+
+    private void updateSlotsDueToPetEdit(Pet removed, Pet added) {
+
+    }
+
+    private void removeExcessSlot(Pet removed,) {
+
+    }
+}
