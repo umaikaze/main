e3e518ae08c075daef5bd4dbba1687920d47f515
Chen Shenghao <chen.shenghao@u.nus.edu>
2020-03-30 15:42:22 +0800

    Add checks and convert to full String-based representation

--- src/main/java/seedu/address/storage/JsonAdaptedSlot.java
+++ src/main/java/seedu/address/storage/JsonAdaptedSlot.java
@@ -1,32 +1,40 @@
 package seedu.address.storage;
 
+import static seedu.address.logic.parser.slot.SlotParserUtil.MESSAGE_INVALID_DATETIME;
+import static seedu.address.logic.parser.slot.SlotParserUtil.MESSAGE_INVALID_DURATION;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.DateTimeUtil;
+import seedu.address.logic.parser.general.exceptions.ParseException;
 import seedu.address.model.PetTracker;
 import seedu.address.model.pet.Name;
-import seedu.address.model.pet.Pet;
 import seedu.address.model.slot.Slot;
 
 /**
  * Jackson-friendly version of {@link Slot}.
  */
 public class JsonAdaptedSlot {
-    private final String pet;
-    private final LocalDateTime dateTime;
-    private final Duration duration;
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Slot's %s field is missing!";
+
+    private final String name;
+    private final String dateTime;
+    private final String duration;
+
 
     /**
      * Constructs a {@code JsonAdaptedSlot} with the given {@code pet}, {@code dateTime} and {@code duration}.
      */
     @JsonCreator
-    public JsonAdaptedSlot(@JsonProperty("pet") String pet, @JsonProperty("dateTime") LocalDateTime dateTime,
-                           @JsonProperty("duration") Duration duration) {
-        this.pet = pet;
+    public JsonAdaptedSlot(@JsonProperty("name") String name, @JsonProperty("dateTime") String dateTime,
+                           @JsonProperty("duration") String duration) {
+        this.name = name;
         this.dateTime = dateTime;
         this.duration = duration;
     }
@@ -35,9 +43,9 @@ public class JsonAdaptedSlot {
      * Converts a given {@code Slot} into this class for Jackson use.
      */
     public JsonAdaptedSlot(Slot source) {
-        pet = source.getPet().getName().fullName;
-        dateTime = source.getDateTime();
-        duration = source.getDuration();
+        name = source.getPet().getName().fullName;
+        dateTime = source.getDateTime().format(DateTimeUtil.DATETIME_FORMAT);
+        duration = String.valueOf(source.getDuration().toMinutes());
     }
 
     /**
@@ -46,7 +54,31 @@ public class JsonAdaptedSlot {
      * @throws IllegalValueException if there were any data constraints violated in the adapted tag.
      */
     public Slot toModelType(PetTracker petTracker) throws IllegalValueException {
-        Name petName = new Name(pet);
-        return new Slot(petTracker.getPet(petName), dateTime, duration);
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+        }
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+        final Name modelName = new Name(name);
+
+        final LocalDateTime modelDateTime;
+        try {
+            modelDateTime = LocalDateTime.parse(dateTime, DateTimeUtil.DATETIME_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new ParseException(MESSAGE_INVALID_DATETIME);
+        }
+
+        final Duration modelDuration;
+        try {
+            modelDuration = Duration.ofMinutes(Long.parseLong(duration));
+        } catch (NumberFormatException e) {
+            throw new ParseException(MESSAGE_INVALID_DURATION);
+        }
+        if (modelDuration.isNegative() || modelDuration.isZero()) {
+            throw new ParseException(MESSAGE_INVALID_DURATION);
+        }
+
+        return new Slot(petTracker.getPet(modelName), modelDateTime, modelDuration);
     }
 }
